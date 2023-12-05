e30db93f24c2408792e247ee86f03d43f27c8104
xpknight <xpknight@gmail.com>
2023-12-05 18:43:48 +0530

    Correct letter is reflected in top word list

--- scripts/script.js
+++ scripts/script.js
@@ -1,14 +1,33 @@
 const keyboardDiv = document.querySelector('.keyboard');
 const wordDisplay = document.querySelector('.word-display');
 
+let curWord = '';
 //getting Random word from WordList
 function getRandomWord() {
     const { word, hint } = wordList[Math.floor(Math.random() * wordList.length)];
     console.log(word, hint);
+    curWord = word;
     document.querySelector('.hint-text b').innerText = hint;
     wordDisplay.innerHTML = word.split('').map((letter) => '<li class="letter" ></li>').join('');
 }
 
+//Check if clicked letter is in word
+function checkLetter(button, clickedLetter) {
+
+    if (curWord.includes(clickedLetter)) {
+        console.log(clickedLetter + '  Found');
+        [...curWord].forEach((letter, index) => {
+            if (letter === clickedLetter) {
+                wordDisplay.children[index].innerText = letter;
+                wordDisplay.children[index].classList.add('guessed');
+            }
+        })
+    } else {
+        console.log(clickedLetter + '  Not Found')
+    }
+}
+
+// checkLetter(curWord);
 getRandomWord()
 
 
@@ -16,8 +35,6 @@ getRandomWord()
 for (let i = 97; i < 123; i++) {
     const button = document.createElement('button');
     button.innerText = String.fromCharCode(i).toUpperCase();
-    button.addEventListener('click', () => {
-        console.log(String.fromCharCode(i));
-    });
+    button.addEventListener('click', (e) => checkLetter(e.target, String.fromCharCode(i)));
     keyboardDiv.appendChild(button);
 } 
