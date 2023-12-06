674fde4897131c74d0d5315a24012c6388d9b2ef
xpknight <xpknight@gmail.com>
2023-12-06 18:42:07 +0530

    Logic Implimentation of Play Again and reset of Data is Completed. Project Complete

--- scripts/script.js
+++ scripts/script.js
@@ -1,48 +1,62 @@
 const hangmanImage = document.querySelector('.hangman-box img');
 const keyboardDiv = document.querySelector('.keyboard');
 const wordDisplay = document.querySelector('.word-display');
-const life_display = document.querySelector('.life-display b');
-let life = 0;
+const display_life = document.querySelector('.life-display b');
+const gameModal = document.querySelector('.game-modal');
+const resetGameBtn = document.querySelector('.play-again');
+
+let wrongGuess, curWord, currectLetters;
 let max_life = 6;
-let curWord = '';
+
 //getting Random word from WordList
 function getRandomWord() {
     const { word, hint } = wordList[Math.floor(Math.random() * wordList.length)];
     console.log(word, hint);
     curWord = word;
     document.querySelector('.hint-text b').innerText = hint;
-    wordDisplay.innerHTML = word.split('').map((letter) => '<li class="letter" ></li>').join('');
+    resetGame();
+}
+
+function gameOver(isWin) {
+    setTimeout(() => {
+        gameModal.classList.add('show');
+        const modalText = (isWin ? 'You Found the Word:' : 'You Lost, The Word was:');
+        gameModal.querySelector('img').src = (isWin ? 'images/victory.gif' : 'images/lost.gif');
+        gameModal.querySelector('h4').innerText = (isWin ? 'Congratulations ' : 'Game Over!');
+        gameModal.querySelector('p').innerHTML = `${modalText} <b>${curWord}</b>`;
+    }, 300);
+}
+
+function resetGame() {
+    wrongGuess = 0;
+    currectLetters = [];
+    hangmanImage.src = `images/hangman-${wrongGuess}.svg`;
+    display_life.innerText = `${wrongGuess}/${max_life}`;
+    [...keyboardDiv.children].forEach(button => button.disabled = false);
+    wordDisplay.innerHTML = curWord.split('').map(() => '<li class="letter" ></li>').join('');
+    gameModal.classList.remove('show');
 }
 
 //Check if clicked letter is in word
 function checkLetter(button, clickedLetter) {
 
     if (curWord.includes(clickedLetter)) {
-        //Show all the currect letter in the Display Word
         [...curWord].forEach((letter, index) => {
             if (letter === clickedLetter) {
+                currectLetters.push(letter);
                 wordDisplay.children[index].innerText = letter;
                 wordDisplay.children[index].classList.add('guessed');
             }
         })
     } else {
-        //When Wronng Letter is clicked decrease life and update heart display     ;
-        life++;
-        hangmanImage.src = `images/hangman-${life}.svg`;
+        wrongGuess++;
+        hangmanImage.src = `images/hangman-${wrongGuess}.svg`;
     }
-    life_display.innerText = `${life}/${max_life}`;
-    //Disable the keyboard key if pressed 
+    display_life.innerText = `${wrongGuess}/${max_life}`;
     button.disabled = true;
-    //Check if all the letter is guessed
-    if (life >= 6) {
-        console.log("Game Lost !!!!!");
-    }
-    if (wordDisplay.innerText === curWord) {
-        console.log("Game Won !!!!!");
-    }
+    if (wrongGuess === max_life) return gameOver(false);
+    if (curWord.length === currectLetters.length) return gameOver(true);
 }
-
-getRandomWord()
 
 //creating Keyboard Buttons 
 for (let i = 97; i < 123; i++) {
@@ -50,4 +64,8 @@ for (let i = 97; i < 123; i++) {
     button.innerText = String.fromCharCode(i).toUpperCase();
     button.addEventListener('click', e => checkLetter(e.target, String.fromCharCode(i)));
     keyboardDiv.appendChild(button);
-} 
+}
+
+//Get FIrst Random Number
+getRandomWord()
+resetGameBtn.addEventListener('click', getRandomWord);
